253d2c9ab8d43685527c7a900c70f404fe822dc4
José Eduardo Margarido de Souza <jeduardoms.2007@gmail.com>
2025-04-22 08:42:26 -0300

    mais uma att

--- src/server.js
+++ src/server.js
@@ -1,10 +1,6 @@
-const express = require('express');
-const app =express();
+import postgres from 'postgres'
 
-app.use(express.json());
-app.get("/", (req,res)=>{
-    res.send("Hello World");
-});
-app.listen(3000, () => {
-    console.log('Porta 3000');
-});
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+export default sql
